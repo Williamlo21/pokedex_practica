d30352f0ca36875c7b46bdd1973f575c1cbbb0c1
Williamlo21 <lopezrebellonw@gmail.com>
2024-04-19 16:11:54 -0500

    new project with npm and bootstrap

---
+++ app/Http/Controllers/PokemonController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pokemon;
+use App\Http\Requests\StorePokemonRequest;
+use App\Http\Requests\UpdatePokemonRequest;
+
+class PokemonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePokemonRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pokemon $pokemon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pokemon $pokemon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePokemonRequest $request, Pokemon $pokemon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pokemon $pokemon)
+    {
+        //
+    }
+}
